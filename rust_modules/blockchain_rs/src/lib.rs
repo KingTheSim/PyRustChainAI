46d51bdf74b8ea64aff56446535e5234b3576475
KingTheSim <KingTheSim@abv.bg>
2023-08-17 22:59:32 +0300

    rust_block_start

--- rust_modules/blockchain_rs/src/lib.rs
+++ rust_modules/blockchain_rs/src/lib.rs
@@ -1,8 +1,52 @@
 use pyo3;
+use chrono::prelude::*;
+use blake2::{Blake2b512, Blake2s256, Digest};
+use bincode;
+use serde::{Deserialize, Serialize};
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn time_getter() -> String {
+    Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
 }
+
+#[derive(Serialize, Deserialize)]
+struct HashWrapper([u8; 64]);
+
+#[derive(Serialize, Deserialize)]
+pub struct Block {
+    index: u64,
+    timestamp: String,
+    proof: u64,
+    previous_hash: HashWrapper,
+
+}
+
+impl Block {
+    fn generator(&self, index: u64, proof: u64, previous_hash: Blake2b512) -> Block {
+        let timestamp = time_getter();
+        Block {
+            index: index + 1,
+            timestamp,
+            proof,
+            previous_hash
+        }
+    }
+
+    fn hasher(&self) -> Blake2b512 {
+        let serialization = bincode::serialize(&self).unwrap();
+        let mut hash = Blake2b512::new();
+        hash.update(&serialization);
+        hash.finalize();
+
+        hash
+        
+    }
+}
+
+pub struct BlockChain {
+    chain: Vec<Block>,
+    length: u64,
+}
+
 
 #[cfg(test)]
 mod tests {
